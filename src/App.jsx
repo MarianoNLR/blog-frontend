6ece7d9d1321a7f0297eedc56301f676620216ce
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-04-30 20:25:52 -0300

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { Home } from './pages/Home.jsx'
 import { Error404 } from './pages/Error404.jsx'
 import { Login } from './pages/Login.jsx'
+import Layout from './components/Layout.jsx'
 
 function App() {
   // eslint-disable-next-line no-unused-vars
@@ -11,11 +12,13 @@ function App() {
 
   return (
     <>
-      <Routes>
-        <Route index path='/' element={<Home />}></Route>
-        <Route path='/login' element={<Login setUser={setUser} />}></Route>
-        <Route path='*' element={<Error404 />}></Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route index path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login setUser={setUser} />}></Route>
+          <Route path='*' element={<Error404 />}></Route>
+        </Routes>
+      </Layout>
     </>
   )
 }
