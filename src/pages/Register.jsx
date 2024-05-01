73452c208b8a046285934c59e263d5732e1c0cd3
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-01 20:10:51 -0300

    register page added

---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "../components/RegisterForm.jsx"
+import './css/Register.css'
+export function Register () {
+  return (
+    <>
+      <main className="main">
+        <RegisterForm></RegisterForm>
+      </main>
+    </>
+  )
+}
