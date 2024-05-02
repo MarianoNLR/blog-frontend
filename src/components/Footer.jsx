053a6661df90ea82418f3e4d39bf543e8519cf64
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-02 20:52:48 -0300

    footer info updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './css/Footer.css'
 export function Footer () {
   return (
     <>
-      <div className="footer-wrapper">Footer</div>
+      <div className="footer-wrapper">Desgined and Developed by Mariano Lotero Rolón</div>
     </>
   )
 }
