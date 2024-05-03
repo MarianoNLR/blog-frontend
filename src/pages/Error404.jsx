4851ac75d070bf4a66fd62aec4119fd9ae682122
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-03 19:31:51 -0300

    update page error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom'
+import './css/Error404.css'
+
 export function Error404 () {
+
   return (
     <>
-      <h1>Error404 Page</h1>
+      <main className="error404-main">
+        <div className='title-wrapper'>
+          <h1>404</h1>
+        </div>
+        <div className='description-wrapper'>
+          <p className='message-text'>Sorry, but we could not find what you are looking for.</p>
+          <p className='link-text'><Link to="/">Click here</Link> to go back.</p>
+        </div>
+      </main>
     </>
   )
 }
