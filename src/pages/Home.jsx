b8c70b18a5bab08c1f5ff6e0b74cb23e1090763e
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-07 19:58:14 -0300

    posts fetch and display list of post card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from 'react'
+import { PostCard } from '../components/PostCard.jsx'
 import './css/Home.css'
 
 export function Home () {
+  const [postList, setPostList] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/posts/`)
+    .then(res => res.json())
+    .then(data => setPostList(data))
+    
+  },[])
+
+
   return (
     <>
-    <main className='main'>
+    <main className='main-home'>
       <h1>Home Page</h1>
+      <div className='post-list-wrapper'>
+        {postList.length > 0 && 
+          postList.map((item, index) =>
+            <PostCard key={index} title={item.title} description={item.description} id={item.id}/>
+        )}
+      </div>
     </main>
     </>
   )
