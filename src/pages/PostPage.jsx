4987bdf4cf71e296504eb76164310cce5177c7eb
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-09 20:37:58 -0300

    update, now using LikeButton component

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
+import { LikeButton } from "../components/LikeButton.jsx"
 import './css/PostPage.css'
 
 export function PostPage () { 
@@ -61,12 +62,7 @@ export function PostPage () {
                         </div>
                         <div className="likes-section">
                             <div className="like-button-wrapper">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" 
-                                stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" 
-                                className="feather feather-heart" onClick={handleLike}>
-                                    <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
-                                    </path>
-                                </svg>
+                                <LikeButton handleLike={handleLike}></LikeButton>
                             </div>
                             <span className="likes-number">{post.likes}</span>
                         </div>
