649306e54ea92322a2b9ed9fbd11a4333d3f6469
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-17 16:10:00 -0300

    + comments list

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,10 +1,26 @@
 import { CommentForm } from "../components/CommentForm.jsx"
+import PropType from 'prop-types'
+import './css/CommentSection.css'
 
-export function CommentSection () { 
+export function CommentSection ({ comments }) { 
+    
     return (
         <div className="comments-section">
             Comments Section
             <CommentForm></CommentForm>
+            <div className="comments-list">
+                {comments.map(comment => (
+                <div key={comment._id} className="comment-wrapper">
+                        <span>{comment.username ? '👤 ' + comment.username : '👤 Unknown'}</span>
+                        <p>{comment.comment}</p>
+                    </div>
+                
+                ))}
+            </div>
         </div>
     )
+}
+
+CommentSection.propTypes = {
+    comments: PropType.array
 }
