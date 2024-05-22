f61a41b3815c4b6e2eb81ebc885d9e132d64cd17
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-22 18:18:51 -0300

    card styles

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,16 +4,23 @@ import { useNavigate } from 'react-router-dom'
 
 export function PostCard ({ item }) {
     const navigate = useNavigate()
-
     const handleClick = (id) => {
         console.log(id)
         navigate(`/post/${id}`)
     }
 
+    const truncateDescription = (p) => {
+        if (p.length > 100) {
+            return p.slice(0, 300) + '...'
+        } else {
+            return p
+        }
+    }
+
     return (
         <div className='post-card-wrapper' onClick={() => handleClick(item.id)}> 
             <h3 className='post-card-title'>{item.title}</h3>
-            <p className='post-card-description'>{item.description}</p>
+            <p className='post-card-description'>{truncateDescription(item.description)}</p>
         </div>
     )
 }
