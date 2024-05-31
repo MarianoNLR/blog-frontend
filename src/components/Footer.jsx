a9143570e343c18c2e8dd6976d00c3e522893e2b
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-31 18:56:59 -0300

    credits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import './css/Footer.css'
 export function Footer () {
   return (
     <>
-      <div className="footer-wrapper">Designed and Developed by Mariano Lotero Rolón</div>
+      <div className="footer-wrapper">
+      <span>Designed and Developed by Mariano Lotero Rolón</span>
+      <br></br>
+      Vectors and icons by <a href="https://www.svgrepo.com" target="_blank">SVG Repo</a>
+      </div>
     </>
   )
 }
