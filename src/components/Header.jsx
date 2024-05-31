e1e511cc61b88516121517d99d9a4176bc7f7301
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-30 22:06:22 -0300

    + logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import './css/Header.css'
 
 export function Header () {
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    //TODO logout function
+    window.localStorage.removeItem('loggedBlogApp')
+    navigate('/login')
+  }
+
   return (
     <>
-      <div className="header-wrapper"><a href='/'>Blog Project</a></div>
+      <div className="header-wrapper">
+        <a href='/' className='header-main-button'>Blog Project</a>
+        <p onClick={handleLogout} className='logout-button'>Logout</p>
+      </div>
     </>
   )
 }
