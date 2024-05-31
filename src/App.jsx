90f735ba331d5b4f6378bfaf8b2cdee0c892d8d6
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-05-31 18:09:11 -0300

    + user profile route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Error404 } from './pages/Error404.jsx'
 import { Login } from './pages/Login.jsx'
 import { Register } from './pages/Register.jsx'
 import {PostPage} from './pages/PostPage.jsx'
+import {Profile} from './pages/Profile.jsx'
 import Layout from './components/Layout.jsx'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Routes>
               <Route index path='/' element={user ? <Home /> : <Navigate to="/login" />}></Route>
               <Route path='/post/:id' element={<PostPage />}></Route>
+              <Route path='/profile' element={<Profile />} ></Route>
               <Route path='/login' element={<Login />}></Route>
               <Route path='/register' element={<Register />}></Route>
               <Route path='*' element={<Error404 />}></Route>
