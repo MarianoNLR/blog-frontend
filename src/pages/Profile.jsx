1ee965b83d93c238944bcaddbadd5e1767eff97b
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-06-05 17:50:00 -0300

    fix posts list

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 import { PostCard } from "../components/PostCard.jsx"
+import './css/Profile.css'
 
 export function Profile () {
     const [user, setUser] = useState({})
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState({posts: []})
     const [currentPage, setCurrentPage] = useState(1)
     const userData = JSON.parse(window.localStorage.getItem('loggedBlogApp'))
 
@@ -23,7 +24,11 @@ export function Profile () {
                 setUser(userJson)
                 const postsRes = await fetch(`http://localhost:3000/posts?userId=${userJson.id}&currentPage=${currentPage}`)
                 const postsData = await postsRes.json()
-                setPosts(postsData)
+                setPosts(prevPostList => ({
+                    totalPages: postsData.totalPages,
+                    page: postsData.page,
+                    posts: [...prevPostList.posts, ...postsData.posts]
+                }))
                 console.log(postsData)
             } catch (error) {
                 console.error(error)
@@ -31,7 +36,7 @@ export function Profile () {
         }
 
         fetchData()
-    }, [])
+    }, [currentPage])
 
     const viewMoreHandle = () => {
         setCurrentPage(currentPage + 1)
@@ -39,27 +44,30 @@ export function Profile () {
 
     return (
         <>
-            <h1>Profile Page</h1>
-            <div className="data-profile-wrapper">
-                <p>Username: {user.username}</p>
-                <p>Name: {user.name}</p>
-            </div>
-            <div className="user-posts-wrapper">
-            {posts.posts && posts.posts.length > 0 ? 
-            posts.posts.map((item, index) =>
-              <PostCard key={index} item={item} />
-            )
-          :  
-            <div><h3>Posts not found.</h3></div> 
-          }
+            
+            <main className="main-profile">
+                <div className="data-profile-wrapper">
+                    <p>Username: <span className="data-profile-value">{user.username}</span></p>
+                    <p>Name: <span className="data-profile-value">{user.name}</span></p>
+                </div>
+                <div className="user-posts-wrapper">
+                    <h2>{user.username}&apos;s Posts</h2>
+                {posts.posts && posts.posts.length > 0 ? 
+                posts.posts.map((item, index) =>
+                <PostCard key={index} item={item} />
+                )
+            :  
+                <div><h3>Posts not found.</h3></div> 
+            }
 
-            <div className='view-more-wrapper'>
-                {posts.totalPages > currentPage 
-                ? <button className='view-more-button' onClick={viewMoreHandle}>View More</button> 
-                : null
-                }  
-            </div>
-            </div>
+                <div className='view-more-wrapper'>
+                    {posts.totalPages > currentPage 
+                    ? <button className='view-more-button' onClick={viewMoreHandle}>View More</button> 
+                    : null
+                    }  
+                </div>
+                </div>
+            </main>
         </>
     )
 }
